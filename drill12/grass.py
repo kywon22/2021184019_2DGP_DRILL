6cbc32d014e33a7f7a208d2f8772abd44ccbf198
kywon22 <kywon622@naver.com>
2022-11-03 16:10:14 +0900

    drill12

---
+++ drill12/grass.py
@@ -0,0 +1,21 @@
+from pico2d import *
+
+class Grass1:
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+    def draw(self):
+        self.image.draw(400, 30)
+
+    def update(self):
+        pass
+
+class Grass2:
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+    def draw(self):
+        self.image.draw(400, 10)
+
+    def update(self):
+        pass
